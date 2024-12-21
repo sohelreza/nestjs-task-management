598277e0837db43e8b21063ffe0c0b7161735268
sohelreza <rezasmsohel@gmail.com>
2024-12-22 03:46:21 +0600

    First Commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform.interceptor';
+
+async function bootstrap() {
+  const logger = new Logger();
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  const port = 4000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
+}
+bootstrap();
---
+++ src/tasks/tasks.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+import { Task } from './task.entity'; // Import the entity (Task) here
+import { TasksRepository } from './tasks.repository'; // Import the custom repository
+import { AuthModule } from 'src/auth/auth.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
+  controllers: [TasksController],
+  providers: [TasksService, TasksRepository], // Register TasksRepository as a provider
+})
+export class TasksModule {}
